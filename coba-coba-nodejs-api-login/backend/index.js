96322315a7931a547a0382bc9a855becbe3c69e9
brwnxt <ihsanulalif12@gmail.com>
2022-06-02 17:57:16 +0700

    memperbaiki index.js

--- coba-coba-nodejs-api-login/backend/index.js
+++ coba-coba-nodejs-api-login/backend/index.js
@@ -22,4 +22,4 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(5000, () => console.log('Server running at port 3000'));
